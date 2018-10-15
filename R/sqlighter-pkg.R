5e2ef1d8ab82b70ef26be6a6374818f8f8d011a0
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-10-15 21:29:06 +0100

    avoid conflict with CRAN's sqliter

---
+++ R/sqlighter-pkg.R
@@ -0,0 +1,9 @@
+#' sqlighter: A package for computating the notorious bar statistic.
+#'
+#' The sqlighter package provides convenient wrappers that safely open and close
+#' the connection to the SQLite database before and after sending queries and
+#' receiving results.
+#'
+#' @docType package
+#' @name sqlighter
+NULL
